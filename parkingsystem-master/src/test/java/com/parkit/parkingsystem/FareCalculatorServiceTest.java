31b317ecbe6de83a5c1a2b1a8ce25239009fd2b5
melisabeth <melisabeth@LCSDSIMEKS.citadelle.local>
2021-05-22 14:26:31 -0400

    Ajout des TU @Mock liées à la remise de 5%

--- parkingsystem-master/src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ parkingsystem-master/src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -2,14 +2,20 @@ package com.parkit.parkingsystem;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.FareCalculatorService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import java.util.Date;
 
@@ -17,16 +23,22 @@ public class FareCalculatorServiceTest {
 
     private static FareCalculatorService fareCalculatorService;
     private Ticket ticket;
+    
+    @Mock
+    private static TicketDAO ticketDAO;
 
     @BeforeAll
     private static void setUp() {
+    	ticketDAO = new TicketDAO();
         fareCalculatorService = new FareCalculatorService();
     }
 
     @BeforeEach
     private void setUpPerTest() {
         ticket = new Ticket();
+       
     }
+    
 
     @Test
     public void calculateFareCar(){
@@ -154,6 +166,41 @@ public class FareCalculatorServiceTest {
         ticket.setParkingSpot(parkingSpot);
         fareCalculatorService.calculateFare(ticket);
         assertEquals( 0 , ticket.getPrice());
+          
+    }
+    
+    @Test
+    public void calculateFareBikeWithDiscount(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );//60 minutes parking time should give an hour parking fare
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        
+        when(ticketDAO.getRecurrentUser(Mockito.anyString())).thenReturn(2);
+
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(0.95 , ticket.getPrice());
+          
+    }
+    @Test
+    public void calculateFareCarWithDiscount(){
+        Date inTime = new Date();
+        inTime.setTime( System.currentTimeMillis() - (  60 * 60 * 1000) );//60 minutes parking time should give an hour parking fare
+        Date outTime = new Date();
+        ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.BIKE,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        
+        when(ticketDAO.getRecurrentUser(Mockito.anyString())).thenReturn(2);
+        
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals( 0.95 , ticket.getPrice());
           
     }
 }
