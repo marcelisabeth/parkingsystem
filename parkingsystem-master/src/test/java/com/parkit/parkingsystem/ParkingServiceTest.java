02ba96a1273de7f389e2bac2ef6855450f88b306
melisabeth <melisabeth@LCSDSIMEKS.citadelle.local>
2021-08-20 12:42:44 -0400

    - Ajout du test incoming vehicule - Augmentation du taux de couverture des tests

--- parkingsystem-master/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ parkingsystem-master/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -1,4 +1,4 @@
-package com.parkit.parkingsystem;
+/* package com.parkit.parkingsystem;
 
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
@@ -57,5 +57,104 @@ public class ParkingServiceTest {
         parkingService.processExitingVehicle();
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
     }
+         
+    @Test
+    public void processIncomingVehicleTest() {
+
+    		when(inputReaderUtil.readSelection()).thenReturn(1);
+    		when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+    		when(ticketDAO.saveTicket(any(Ticket.class))).thenReturn(true);
+    		Date inTime = new Date();
+    		inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
+    		parkingService.processIncomingVehicle();
+    	
+    }	
+}
+
+*/
+
+package com.parkit.parkingsystem;
+
+import com.parkit.parkingsystem.constants.ParkingType;
+import com.parkit.parkingsystem.dao.ParkingSpotDAO;
+import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.model.ParkingSpot;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * <b>ParkingServiceTest is built to unit test ParkingService.</b>
+ *
+ * @author
+ * @version 3.0
+ */
+@ExtendWith(MockitoExtension.class)
+public class ParkingServiceTest {
+
+private static ParkingService parkingService;
+
+@Mock
+private static InputReaderUtil inputReaderUtil;
+@Mock
+private static ParkingSpotDAO parkingSpotDAO;
+@Mock
+private static TicketDAO ticketDAO;
+
+@BeforeEach
+private void setUpPerTest() {
+try {
+when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+
+when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
+
+parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+} catch (Exception e) {
+e.printStackTrace();
+throw new RuntimeException("Failed to set up test mock objects");
+}
+}
+
+@Test
+@DisplayName("After process incoming vehicule, system should update parking spot available and save ticket")
+public void processIncomingVehicleTest() {
+
+when(inputReaderUtil.readSelection()).thenReturn(1);
+when(parkingSpotDAO.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+when(ticketDAO.saveTicket(any(Ticket.class))).thenReturn(true);
+Date inTime = new Date();
+inTime.setTime(System.currentTimeMillis() - (60 * 60 * 1000));
+parkingService.processIncomingVehicle();
+
+verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+verify(ticketDAO, times(1)).saveTicket(any(Ticket.class));
+}
+
+@Test
+@DisplayName("After process exiting vehicule, system should update parking spot available")
+public void processExitingVehicleTest() {
+
+ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
+Ticket ticket = new Ticket();
+ticket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
+ticket.setParkingSpot(parkingSpot);
+ticket.setVehicleRegNumber("ABCDEF");
+when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
+parkingService.processExitingVehicle();
+
+verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+}
 
 }
